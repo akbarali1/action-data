4e04b76c6836f4065387f69338c39b44842edbab
Akbarali <39323182+akbarali1@users.noreply.github.com>
2024-04-29 21:18:18 +0500

    addValidationValue method add

--- src/ActionDataBase.php
+++ src/ActionDataBase.php
@@ -121,6 +121,13 @@ abstract class ActionDataBase implements ActionDataContract
 		}
 	}
 	
+	public function addValidationValue(string $method, array $fields): void
+	{
+		foreach ($fields as $field) {
+			$this->addValidationRule($field, $method);
+		}
+	}
+	
 	/**
 	 * @param  bool  $silent
 	 * @throws ValidationException
